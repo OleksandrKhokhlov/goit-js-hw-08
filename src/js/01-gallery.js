c3ba254e86da35f175f1b0fd4b9d9a0eb1f35312
OleksandrKhokhlov <118597434+OleksandrKhokhlov@users.noreply.github.com>
2023-06-14 20:06:52 +0300

    correct HW8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,8 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import 'simplelightbox/dist/simple-lightbox.esm';
+var lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
 console.log(galleryItems);
